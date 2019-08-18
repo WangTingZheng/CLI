77122e9ff34c7b2e5cf24f96d69d80bbca50fd9f
Engou <2251985371@qq.com>
2019-08-18 15:52:49 +0800

    v2.1.0 更新了： 1、修正完了客户端；

--- Server/Client.js
+++ Server/Client.js
@@ -1,10 +1,10 @@
+#!/usr/bin/env node
 var net = require('net');
 var client = new net.Socket();
 client.setEncoding('utf8');
 client.connect(3000,'192.168.0.102',function(){
       console.log('connect');
       client.write('to server');
-      client.end('end');
 });
 client.on('data',function(data){
       console.log('receive data from server');
