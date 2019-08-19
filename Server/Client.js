69af35e8318d54724fedc887ce00f5aa84e45872
Engou <2251985371@qq.com>
2019-08-19 19:36:57 +0800

    v2.3.0 更新了： 1、更nb的json操作函数 2、更换json的格式

--- Server/Client.js
+++ Server/Client.js
@@ -2,15 +2,17 @@
 var net = require('net');
 var json =require('../json.js');
 
+console.log("running...")
 var client = new net.Socket();
 client.setEncoding('utf8');
+
 client.connect(3000,'192.168.0.102',function(){
-      console.log('connect');
-      while(json.readJson(0,0)=="true"){
-            for(var u=0;u<2;u++){
-                  if(json.readJson[u][1]=="true")
-                  client.write("pin %s is %s\n",u,json.readJson[u][0])
+      console.log("connecting...")
+      while(json.readJson(1,"status")=="true"){
+            for(var i=2;i<=4;i++){
+                  client.write("Pin %s is %s",i,json.readJson(i,"status"))
             }
+            console.log("send done!")
       }
 });
 client.on('data',function(data){
--- test.js
+++ test.js
@@ -1,20 +1,2 @@
-#!/usr/bin/env node
-var fs=require('fs');
-var file="./data.json";
-var result=JSON.parse(fs.readFileSync( file));
-var x=[]
-function dataInit(){
-    var value=[]
-    for(var i=0;i<2;i++){
-        value[i]=result[i+1];
-        console.log(value[i]);
-    }
-    return value
-}
-
-x=dataInit();
-for(var y=0;y<2;y++){
-    if(x[y][1]=='yes'){
-          console.log("send pin %s, is %s",y+1,x[y][0])
-    }
-}
+var v=require('./json.js')
+console.log(v.readJson(1,"status"))
